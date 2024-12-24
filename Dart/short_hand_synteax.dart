ab74051f875e9a136b91d4cd615daa855ca8857a
Dhruv-M-Sathavara <dhruvms1234@gmail.com>
2024-12-25 00:22:44 +0530

    short function

---
+++ Dart/short_hand_synteax.dart
@@ -0,0 +1,9 @@
+void main(){
+  double length = 4.0;
+  double breadth = 5.0;
+
+  print(arearactangele(length, breadth));
+}
+
+
+double arearactangele(double length, double breadth) => length * breadth;
